841823b7a315d36a01f3d2f5a40772d05d6b296a
eastfox2002 <eastfox2002@163.com>
2013-03-29 17:02:04 +0800

    test pullrequest

---
+++ work/demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '9a2d15675da71f0130e61eadffae6afdb91cf1c647fe71a345dbe43cb3ee9037b78212fcb915ea285647dc3faefc78d648179a650da35abe4271535b04a3b7e6'
